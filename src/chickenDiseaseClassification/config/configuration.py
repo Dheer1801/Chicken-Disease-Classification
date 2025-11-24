090a09cddd678ee9d14268e8bcb961e9ba720d90
dheer1801 <dheer1801@gmail.com>
2025-11-24 21:55:54 +0530

    Model evaluation added

--- src/chickenDiseaseClassification/config/configuration.py
+++ src/chickenDiseaseClassification/config/configuration.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 from chickenDiseaseClassification.utils.common import read_yaml, create_directories
 from chickenDiseaseClassification.entity.config_entity import (DataIngestionConfig, 
+                                                               EvaluationConfig, 
                                                                PrepareBaseModelConfig, 
                                                                PrepareCallbacksConfig, 
                                                                TrainingConfig)
@@ -98,3 +99,15 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+
+
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
